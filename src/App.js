53a5c7aeb11142ed517d24bca3a1ed9df9b13d3a
dhavalkavar <dhavalkavar5@gmail.com>
2021-10-12 19:44:01 +0530

    css styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import react from "react";
+import react, { useState } from "react";
 
 const App = () => {
+  const [count, setcount] = useState(0);
+  const pluscount = () => {
+    setcount(count + 1);
+  };
+  const minuscount = () => {
+    if (count > 0) {
+      setcount(count - 1);
+    } else {
+      alert("zero limit reached");
+      setcount(0);
+    }
+  };
   return (
     <>
       <div className="main_div">
         <div className="center_div">
-          <h1>0</h1>
-          <button>increm</button> <button>decrem</button>
+          <h1>{count}</h1>
+          <div className="btn_div">
+            <button onClick={pluscount}>increm</button>{" "}
+            <button onClick={minuscount}>decrem</button>
+          </div>
         </div>
       </div>
     </>
